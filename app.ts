d63ef24d8c86249d71f7e0c3b8a75efdb8bcf85f
Sivinskikh Mikhail <mikhail.sivinskikh@gmail.com>
2023-02-09 22:52:50 +0200

    self-built array and hashTable classes added

---
+++ app.ts
@@ -0,0 +1,26 @@
+// const string: string = "Hi, my name is Mikhail";
+
+// function reverseString(string: string): string {
+//   return string.split("").reverse().join("");
+// }
+
+// console.log(reverseString(string));
+
+// const mergeSortedArrays = (arr1: number[], arr2: number[]): number[] => {
+//   return [...arr1, ...arr2].sort((a, b) => a - b);
+// };
+
+// console.log(mergeSortedArrays([1, 2, 3, 66], [4, 5, 115]));
+
+// function twoSum(nums: number[], target: number): number[] {
+//   const numbersObj: { [key: number]: any } = {};
+//   for (const iterator in nums) {
+//     if (numbersObj[nums[iterator]]) {
+//       return [iterator, numbersObj[nums[iterator]]];
+//     }
+//     numbersObj[target - nums[iterator]] = iterator;
+//   }
+//   return [];
+// }
+
+// console.log(twoSum([2, 7, 11, 15], 9));
