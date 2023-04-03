39c975831ef7b160abe2e89ef94d85978a3b8377
Sivinskikh Mikhail <mikhail.sivinskikh@gmail.com>
2023-04-03 20:59:46 +0300

    dynamic programming memoization added fibonacci memoized

--- src/dynamicProgramming/memoization.ts
+++ src/dynamicProgramming/memoization.ts
@@ -12,3 +12,20 @@ function memoizedAdd80() {
 
 let memoized: (n: number) => number = memoizedAdd80();
 memoized(1);
+
+function fibonacciMaster() {
+  //O(n)
+  let cache: { [key: number]: number } = {};
+  return function fib(n: number) {
+    if (n in cache) {
+      return cache[n];
+    } else {
+      if (n < 2) {
+        return n;
+      } else {
+        cache[n] = fib(n - 1) + fib(n - 2);
+        return cache[n];
+      }
+    }
+  };
+}
